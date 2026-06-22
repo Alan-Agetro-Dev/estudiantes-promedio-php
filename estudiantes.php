66db597149bba9e81c0f52fe1070b1b8d659929c
Alan-Agetro-Dev <alanagetrodev@protonmail.com>
2026-06-21 21:49:56 -0600

    Se le agrego atributo calificaciones y un metodo calcularPromedio

--- estudiantes.php
+++ estudiantes.php
@@ -2,10 +2,17 @@
 class Estudiante {
     public $nombre;
     public $edad;
+    public $calificaciones;
 
-    public function __construct($nombre, $edad) {
+    public function __construct($nombre, $edad, $calificaciones) {
         $this->nombre = $nombre;
         $this->edad = $edad;
+        $this->calificaciones = $calificaciones;
+    }
+
+    // Método para calcular promedio
+    public function calcularPromedio() {
+        return array_sum($this->calificaciones) / count($this->calificaciones);
     }
 }
 ?>
