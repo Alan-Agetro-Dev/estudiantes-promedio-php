7a5424a5dabaca4075f733dd2dda62d9bf9a1b1b
Alan-Agetro-Dev <alanagetrodev@protonmail.com>
2026-06-24 22:39:46 -0600

    Proyecto terminado con multiples estudiantes

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require_once "Estudiante.php";
 
 $estudiante1 = new Estudiante("Alan", 20, [90, 80, 85]);
+$estudiante2 = new Estudiante("Maria", 22, [95, 88, 92]);
+$estudiante3 = new Estudiante("Luis", 19, [70, 75, 80]);
 
 $estudiante1->mostrarInfo();
+$estudiante2->mostrarInfo();
+$estudiante3->mostrarInfo();
 ?>
