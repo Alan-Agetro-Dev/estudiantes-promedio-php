d1b8e4a5563431eda0e15402b5b8aabf7cdbf66d
Alan-Agetro-Dev <alanagetrodev@protonmail.com>
2026-06-24 23:13:54 -0600

    Correcion en el llamado al archivo estudiantes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "Estudiante.php";
+require_once "estudiantes.php";
 
 $estudiante1 = new Estudiante("Alan", 20, [90, 80, 85]);
 $estudiante2 = new Estudiante("Maria", 22, [95, 88, 92]);
