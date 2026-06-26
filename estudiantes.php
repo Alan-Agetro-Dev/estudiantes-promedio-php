a69f1e2743e0a5669afe087e5f56368368adedd0
Alan-Agetro-Dev <alanagetrodev@protonmail.com>
2026-06-25 22:37:28 -0600

    Se documento estudiantes-php

--- estudiantes.php
+++ estudiantes.php
@@ -1,20 +1,24 @@
 <?php
+// Clase estudiante
 class Estudiante {
+
+// Atributos
     public $nombre;
     public $edad;
     public $calificaciones;
-
+    
+// Constructor de la clase
     public function __construct($nombre, $edad, $calificaciones) {
         $this->nombre = $nombre;
         $this->edad = $edad;
         $this->calificaciones = $calificaciones;
     }
-
+// Metodo para calcular el promedio
     public function calcularPromedio() {
         return array_sum($this->calificaciones) / count($this->calificaciones);
     }
 
-    // Nuevo método
+    // Metodo para mostrar informacion
     public function mostrarInfo() {
         echo "<h3>Estudiante</h3>";
         echo "Nombre: $this->nombre <br>";
