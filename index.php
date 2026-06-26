380e27eaa41f5bfe58b921e1736eeed0d338b657
Alan-Agetro-Dev <alanagetrodev@protonmail.com>
2026-06-25 22:41:02 -0600

    Se documento index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+// Enlace al archivo estudiantes-php
 require_once "estudiantes.php";
-
+// Instanciacion de clases
 $estudiante1 = new Estudiante("Alan", 20, [90, 80, 85]);
 $estudiante2 = new Estudiante("Maria", 22, [95, 88, 92]);
 $estudiante3 = new Estudiante("Luis", 19, [70, 75, 80]);
-
+// Peticion de metodos
 $estudiante1->mostrarInfo();
 $estudiante2->mostrarInfo();
 $estudiante3->mostrarInfo();
